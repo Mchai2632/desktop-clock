75dbda0fab579df164848d2eac76d00f459e1807
Mchai2632 <kimfei2014@gmail.com>
2025-03-08 21:24:24 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const timeElement = document.getElementById("time");
+const countryElement = document.getElementById("country");
+const dateElement = document.getElementById("date");
+
+function getTimeAndDate() {
+    const time = dayjs().format("HH:mm:ss");
+    const date = dayjs().format("YYYY-MM-DD");
+    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    return [time, date, timeZone];
+}
+
+setInterval(() => {
+    const [time, date, timeZone] = getTimeAndDate();
+
+    countryElement.innerText = timeZone;
+    timeElement.innerText = time;
+    dateElement.innerText = date;
+}, 1000);
